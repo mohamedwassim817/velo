2c73d1c3b79e5804ac31b4305ed3d6a8eb6dfbdf
mohamed wassim gabtni <mohamedwassim.gabtni1@esprit.tn>
2020-02-19 02:51:00 +0100

    first commit

---
+++ src/ProduitBundle/Entity/Categorie.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace ProduitBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+/**
+ * Categorie
+ *
+ * @ORM\Table(name="categorie")
+ * @ORM\Entity(repositoryClass="ProduitBundle\Repository\CategorieRepository")
+ */
+class Categorie
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *@Assert\Length(
+     *      min = 2,
+     *      max = 50,
+     *      minMessage = "Your product name must be at least {{ limit }} characters long",
+     *      maxMessage = "Your product name cannot be longer than {{ limit }} characters"
+     * )
+     * @Assert\NotBlank
+     *  @Assert\Regex(
+     *     pattern="/\d/",
+     *     match=false,
+     *     message="Your name cannot contain a number"
+     * )
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *@Assert\Length(
+     *      min = 2,
+     *      max = 50,
+     *      minMessage = "Your product name must be at least {{ limit }} characters long",
+     *      maxMessage = "Your product name cannot be longer than {{ limit }} characters"
+     * )
+     * @Assert\NotBlank
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Categorie
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Categorie
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
+
